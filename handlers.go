7d058a34d3f588751971bb8b2d902facd9b500ff
Aaron Torgerson <taaron@taaron-a01.vmware.com>
2021-08-05 14:50:08 -0700

    logging

--- handlers.go
+++ handlers.go
@@ -16,7 +16,7 @@ const (
 
 // Healthz return ok without auth for testing connectivity
 func Healthz(w http.ResponseWriter, r *http.Request) {
-
+	log.Println("Health request received")
 	w.WriteHeader(200)
 	data := []byte("OK")
 	w.Write(data)
@@ -27,6 +27,7 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 // TODO, after prototype want data to go elsewhere and then be sent to elastic
 func PostExample(w http.ResponseWriter, r *http.Request) {
 
+	log.Println("PostExample received")
 	payload := Payload{
 		Code:       success,
 		W:          w,
@@ -47,7 +48,7 @@ func (p *Payload) completeRequest() {
 		p.Code = failure
 		log.Println("Error: ", p.Error)
 	}
-
+	log.Println("Request completed")
 	p.W.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	p.W.WriteHeader(p.Code)
 	p.W.Write(p.Data)
